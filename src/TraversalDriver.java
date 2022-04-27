10f6e886a0d06753d159a94fc670ea9e79f982aa
alan perez <perezalan012400@gmail.com>
2022-04-26 22:39:44 -0700

    testing ArrayQueue in Driver class

--- src/TraversalDriver.java
+++ src/TraversalDriver.java
@@ -1,4 +1,20 @@
 public class TraversalDriver
 {
+    public static void main(String[] args)
+    {
+        QueueInterface<String> myQueue = new ArrayQueue<>();
+        myQueue.enqueue("alan");
+        myQueue.enqueue("aldo");
+        myQueue.enqueue("aram");
+        myQueue.enqueue("adriel");
+
+        myQueue.dequeue();
+
+        System.out.println("Front is: " + myQueue.getFront());
+        myQueue.clear();
+        System.out.println();
+
+    }
+
 
 }
